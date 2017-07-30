e2cfa940801c50be7453f170754b421b738a55f8
Kedar Marathe <kmarathe@us.ibm.com>
2017-07-30 04:49:30 -0500

    Method to get all weather alerts along a route

--- weather/services.py
+++ weather/services.py
@@ -31,8 +31,23 @@ def getAlertByLatLong(lat, lon, time):
 def checkAlert(forecast, future_time_ms):
     precip_in = forecast['hour'][datetime.fromtimestamp(future_time_ms/1000).hour]['precip_in']
     # logger.info(">>>>precipitation in inches is: %s", str(precip_in))
-    return precip_in
-    # if float(precip_in) > 0.0:
-    #     return True
-    # else:
-    #     return False
+    # return precip_in
+    if float(precip_in) > 0.0:
+        return True
+    else:
+        return False
+
+def getAllAlertsOnRoute(data):
+    retArray = []
+    for loc in data:
+        location = json.dumps(loc)
+        lat = float(json.loads(location)['latitude'])
+        lon = float(json.loads(location)['longitude'])
+        time = "0.0"
+        print("{\nlat: ",lat)
+        print("\nlon: ",lon)
+        print("\n}")
+        if(getAlertByLatLong(lat,lon,time) is True):
+            retArray.append({'latitude': lat, 'longitude': lon, 'alert': 'Raining here. Roads might be wet. Drive Carefully.'})
+
+    return retArray
